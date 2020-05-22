5eedd60b72d7be40f53c0f22da013376e117770c
xiaohai-huang <n10172912@qut.edu.au>
2020-05-22 17:59:04 +0800

    Designed first two tabs

--- src/ControlPanel/ControlPanel.java
+++ src/ControlPanel/ControlPanel.java
@@ -4,14 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.HashMap;
 
-public class ControlPanel extends JFrame{
+public class ControlPanel extends JFrame {
 
-    private static final int WIDTH = 400;
+    private static final int WIDTH = 800;
 
-    private static final int HEIGHT = 400;
+    private static final int HEIGHT = 600;
 
 
-    public ControlPanel(){
+    public ControlPanel() {
         super("Billboard Control Panel");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         showLoginScreen();
@@ -19,7 +19,7 @@ public class ControlPanel extends JFrame{
     }
 
 
-    private void showLoginScreen(){
+    private void showLoginScreen() {
         JTextField username = new JTextField();
         JTextField password = new JPasswordField();
         Object[] message = {
@@ -33,35 +33,36 @@ public class ControlPanel extends JFrame{
             String name = username.getText();
             String word = password.getText();
             // verify the userName and password
-            if(!verify(name,word)){
-                JOptionPane.showMessageDialog(this,"Authentication failed!",
-                        "Fail to login",JOptionPane.ERROR_MESSAGE);
+            if (!verify(name, word)) {
+                JOptionPane.showMessageDialog(this, "Authentication failed!",
+                        "Fail to login", JOptionPane.ERROR_MESSAGE);
                 System.exit(1);
+            } else {
+                JOptionPane.showMessageDialog(this, "Authentication success!");
             }
-            else{
-                JOptionPane.showMessageDialog(this,"Authentication success!");
-            }
-        }
-        else {// press cancel or close the window
+        } else {// press cancel or close the window
             System.exit(1);
         }
     }
 
-    private void showMainScreen(){
-        this.setVisible(true);
-        this.setSize(new Dimension(WIDTH,HEIGHT));
+    private void showMainScreen() {
+
+        this.setSize(new Dimension(WIDTH, HEIGHT));
         this.setLocationRelativeTo(null);
+        this.getContentPane().add(new ControlPanelUI().getMainTabPanel());
+        this.setVisible(true);
     }
-    private UM getUsers(){
+
+    private UM getUsers() {
         // todo: connect to server to get users
         HashMap<String, User> users = new HashMap<String, User>();
         User u = new User("xiaohai");
-        users.put(u.getUserName(),u);
+        users.put(u.getUserName(), u);
 
         return new UM(users);
     }
 
-    private Boolean verify(String userName, String password){
+    private Boolean verify(String userName, String password) {
         // todo: connect to server
         return true;
     }
